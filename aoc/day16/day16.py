203631a25f01fe989b0f612d42e244c781defbf9
Ron Sprenkels <ron.sprenkels@gmail.com>
2019-11-30 23:09:24 +0100

    2018 day 16 completed.

--- aoc/day16/day16.py
+++ aoc/day16/day16.py
@@ -10,7 +10,7 @@ class Cpu():
                         'banr', 'bani', 'borr', 'bori',
                         'setr', 'seti', 'gtir', 'gtri',
                         'gtrr', 'eqir', 'eqri', 'eqrr']
-        self.num_to_opcode = defaultdict(set)
+        self.num_to_opcode = defaultdict(lambda: set(self.opcodes))
 
     def all_opcodes(self):
         return self.opcodes
@@ -18,8 +18,12 @@ class Cpu():
     def set_regs(self, A, B, C, D):
         self.regs = [A, B, C, D]
 
+    def exec_num(self, opcode_num, A, B, C):
+        return self.exec(next(iter(self.num_to_opcode[opcode_num])), A, B, C)
+
     def exec(self, opcode, A, B, C):
         self.regs = self.dry_exec(opcode, A, B, C)
+        return self.regs
 
     def dry_exec(self, opcode, A, B, C):
         regs = copy.deepcopy(self.regs)
@@ -62,6 +66,9 @@ def test_1():
     cpu = Cpu()
     cpu.set_regs(3, 2, 1, 1)
     assert cpu.dry_exec('addi', 2, 1, 2) == [3, 2, 2, 1]
+    assert cpu.regs == [3, 2, 1, 1]
+    assert cpu.exec('addi', 2, 1, 2) == [3, 2, 2, 1]
+    assert cpu.regs == [3, 2, 2, 1]
 
 
 if __name__ == '__main__':
@@ -82,7 +89,27 @@ if __name__ == '__main__':
 
         if len(matching_opcodes) >= 3:
             opcodes_more_than_three += 1
+        cpu.num_to_opcode[instruction[0]] = matching_opcodes.intersection(cpu.num_to_opcode[instruction[0]])
         index += 4
         if not lines[index].startswith('Before'):
             break
     print(f"day 16 part 1: opcodes more than three: {opcodes_more_than_three}")
+
+    numbers_with_one_opcode = [k for k in cpu.num_to_opcode.keys() if len(cpu.num_to_opcode[k]) == 1]
+    while len(numbers_with_one_opcode) < len(cpu.opcodes):
+        for is_resolved in numbers_with_one_opcode:
+            for k in cpu.num_to_opcode.keys():
+                if k != is_resolved:
+                    cpu.num_to_opcode[k].discard(next(iter(cpu.num_to_opcode[is_resolved])))
+        numbers_with_one_opcode = [k for k in cpu.num_to_opcode.keys() if len(cpu.num_to_opcode[k]) == 1]
+
+    while lines[index] == '\n':
+        index += 1
+
+    cpu.set_regs(0, 0, 0, 0)
+    for line in lines[index:]:
+        print(f"before: {cpu.regs} ", end='')
+        instruction = list(map(int, line.split()))
+        cpu.exec_num(*instruction)
+        print(f"instruction {instruction}  after: {cpu.regs}")
+    print(f"day 16 part 2: {cpu.regs[0]}")
