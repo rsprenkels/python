350fe87cf4ad02ca9c1020df72fbb9e4e070d0f7
Ron Sprenkels <ron.sprenkels@gmail.com>
2019-12-31 00:41:05 +0100

    aoc2019 day 20 part 1.

--- aoc/2019/aoc2019_20.py
+++ aoc/2019/aoc2019_20.py
@@ -1,3 +1,4 @@
+import math
 from collections import defaultdict
 from typing import Dict
 from aoc import P
@@ -46,6 +47,33 @@ def readmaze(lines: str) -> Dict:
     find_labels_y(labels, maze, at_col=xmax - donut_width - 3, offset=2)
     return maze, labels
 
+
+def shortest(maze, labels):
+    teleport = dict()
+    for a, b in  (labels[x] for x in labels if len(labels[x]) == 2):
+        teleport[a] = b
+        teleport[b] = a
+    queue = []
+    seen = set()
+    queue.append((labels['AA'][0], 0))
+    mindist = math.inf
+    while queue:
+        p, dist = queue.pop(0)
+        seen.add(p)
+        reachable = p.nesw()
+        if p in teleport:
+            reachable.append(teleport[p])
+        for px in reachable:
+            if maze[px] == '.':
+                if px == labels['ZZ'][0]:
+                    if dist + 1 < mindist:
+                        mindist = dist + 1
+                else:
+                    if px not in seen:
+                        queue.append((px, dist + 1))
+    return mindist
+
+
 def test_readmaze_1():
     lines = """
          A           
@@ -69,3 +97,10 @@ FG..#########.....#
              Z       
 """.split('\n')[1:-1]
     maze, labels = readmaze(lines)
+    assert shortest(maze, labels) == 23
+
+def test_part_1():
+    with open('aoc2019_20_input.txt', 'r') as f:
+        lines = f.read().split('\n')
+    maze, labels = readmaze(lines)
+    assert shortest(maze, labels) == 632
