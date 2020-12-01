e01ef06cd3a7616dbc09301682c17ded12265f49
Ron Sprenkels <ron.sprenkels@gmail.com>
2020-12-01 22:40:06 +0100

    after watching a video from a German guy.

--- aoc/2020/2020_01.py
+++ aoc/2020/2020_01.py
@@ -1,3 +1,6 @@
+import math
+from itertools import combinations
+
 number_lines = open('2020_01.txt', 'r').readlines()
 
 numbers = []
@@ -15,3 +18,16 @@ for a in range(len(numbers) - 2):
         for c in range(b+1, len(numbers)):
             if numbers[a] + numbers[b] + numbers[c]== 2020:
                 print(f"2020_01 part 2: {numbers[a] * numbers[b] * numbers[c]} ")
+
+def read_input():
+    with open('2020_01.txt') as f:
+        return [int(line) for line in f]
+
+def solve(puzzle, n):
+    for c in combinations(puzzle, n):
+        if sum(c) != 2020: continue
+        return math.prod(c)
+
+puzzle = read_input()
+print(f'part 1: {solve(puzzle, 2)}')
+print(f'part 2: {solve(puzzle, 3)}')
