5a7da8dbc170ed258402ec5482e47449a34cbe30
Ron Sprenkels <ron.sprenkels@gmail.com>
2020-12-06 14:18:31 +0100

    aoc 2020 day 6 after nice refactor

--- aoc/2020/2020_06.py
+++ aoc/2020/2020_06.py
@@ -1,6 +1,5 @@
 import functools
 
-
 def read_input():
     with open('2020_06.txt') as f:
         lines =  [line.rstrip() for line in f.readlines()]
@@ -15,28 +14,16 @@ def read_input():
                 lines.pop(0)
         return result
 
-def solve_part1(puzzle):
-    group_answers = []
-    for group in puzzle:
-        answers = set()
-        for person in group:
-            for answer in person:
-                answers.add(answer)
-        group_answers.append(answers)
-    return sum(len(ga) for ga in group_answers)
-
-def solve_part2(puzzle):
+def solve(puzzle, reduce_func):
     group_answers = []
     for group in puzzle:
         persons_answers = []
         for person in group:
             persons_answers.append({a for a in person})
-        one_group = functools.reduce(lambda a, b : a & b, persons_answers)
+        one_group = functools.reduce(reduce_func, persons_answers)
         group_answers.append(one_group)
     return sum(len(og) for og in group_answers)
 
 puzzle = read_input()
-print(puzzle)
-print(f'part 1: {solve_part1(puzzle)}') # 7128
-print(f'part 2: {solve_part2(puzzle)}') # 3640
-
+print(f'part 1: {solve(puzzle, reduce_func=lambda a, b : a | b)}') # 7128
+print(f'part 2: {solve(puzzle, reduce_func=lambda a, b : a & b)}') # 3640
